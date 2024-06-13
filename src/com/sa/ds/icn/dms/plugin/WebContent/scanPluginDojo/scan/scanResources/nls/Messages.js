fd10b2690ad6f065e58ce4b3c2417941bb5dd2ae
AhmedMahmoudICN <155892968+AhmedMahmoudICN@users.noreply.github.com>
2024-06-13 09:53:45 +0300

    adding project files

---
+++ src/com/sa/ds/icn/dms/plugin/WebContent/scanPluginDojo/scan/scanResources/nls/Messages.js
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+define({
+	root : ({
+		add_document_scan_label : "Scan Document",
+		add_document:"Add Document",
+		add_document_scan_button : "Scan",
+		add_document_lable:"Add Scanned Document",
+		add_document_scan_entery_label : "Scan Document by Using Entry Template",
+		modify_image : "Modify image",
+		no_twain_scanner : "No TWAIN compatible drivers detected.",
+		no_scanner_driver : "No Scanner Driver is Installed",
+		you_can : ">You can:",
+		select_source : "Select Source:",
+		scanner_setting : "Scanner setting",
+		new_height : "New Height :",
+		new_width : "New Width :",
+		interpolation_method : "Interpolation method: ",
+		ok : "Ok",
+		cancel : "Cancel",
+		remove_selected_images : "Remove Selected",
+		remove_all_images : "Remove All",
+		back : "Back",
+		preview_mode : "Preview Mode",
+		ShowUI : "Show UI&nbsp",
+		ADF : "AutoFeeder&nbsp;",
+		Duplex : "Duplex",
+		Pixel_Type : "Pixel Type:",
+		BW : "B&amp;W",
+		Gray : "Gray",
+		RGB : "Color",
+		Resolution : "Resolution:",
+		dir : "ltr",
+		document_scan_type :"Attament Type: ",
+		no_scanned_page:"There is no scanned page",
+	}),
+	"en" : true,
+	"ar" : true
+});
+
